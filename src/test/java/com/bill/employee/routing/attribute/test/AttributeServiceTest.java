94ffcc05e51a147aac673ea4d39da79d6e5baa92
billMarantos1991 <vasileios.marados@gmail.com>
2021-04-02 14:37:23 +0300

    new test package

--- src/test/java/com/bill/employee/routing/attribute/test/AttributeServiceTest.java
+++ src/test/java/com/bill/employee/routing/attribute/test/AttributeServiceTest.java
@@ -1,4 +1,4 @@
-package com.bill.employee.routing.employee.routing;
+package com.bill.employee.routing.attribute.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
