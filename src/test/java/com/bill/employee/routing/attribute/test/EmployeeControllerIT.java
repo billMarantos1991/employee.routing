547f4c0b21c75fe2bc0674088c3625001244a20c
Bill Marantos <75279124+billMarantos1991@users.noreply.github.com>
2021-04-16 21:21:14 +0300

    first integration test

---
+++ src/test/java/com/bill/employee/routing/attribute/test/EmployeeControllerIT.java
@@ -0,0 +1,19 @@
+package com.bill.employee.routing.attribute.test;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import com.bill.employee.routing.controller.EmployeeController;
+
+@SpringBootTest(classes =EmployeeController.class)
+public class EmployeeControllerIT {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
