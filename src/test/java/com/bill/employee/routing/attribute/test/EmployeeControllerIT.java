23b99970a1386f11c3792da863faf32df4ec9a3d
Bill Marantos <75279124+billMarantos1991@users.noreply.github.com>
2021-04-16 22:15:01 +0300

    more test

--- src/test/java/com/bill/employee/routing/attribute/test/EmployeeControllerIT.java
+++ src/test/java/com/bill/employee/routing/attribute/test/EmployeeControllerIT.java
@@ -2,18 +2,54 @@ package com.bill.employee.routing.attribute.test;
 
 
 
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.bill.employee.routing.Application;
 
-import com.bill.employee.routing.controller.EmployeeController;
-
-@SpringBootTest(classes =EmployeeController.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes =Application.class,webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerIT {
+	
+	@LocalServerPort
+	private int port;
 
 	@Test
 	public void test() {
 	
+		String url =  "http://localhost:"+port+"/api/employees";
+		url = url + "/33"; 
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		List<MediaType> mediaTypes =new ArrayList<>();
+		mediaTypes.add(MediaType.APPLICATION_JSON);
+		headers.setAccept( mediaTypes );
+		
+		HttpEntity<String> httpEntity = new HttpEntity<String>("parameters",headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET,httpEntity,String.class);
+		
+		System.out.println("response: " +response.getBody());
+		
+		assertTrue(response.getBody().contains("\"firstName\":\"Χρήστος\""));
 	}
 
 }
