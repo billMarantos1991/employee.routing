3f2cc329d65d25eeda6bd6bca9785bdc2218dee8
Bill Marantos <75279124+billMarantos1991@users.noreply.github.com>
2021-04-19 15:34:03 +0300

    More integration test

--- src/test/java/com/bill/employee/routing/attribute/test/EmployeeControllerIT.java
+++ src/test/java/com/bill/employee/routing/attribute/test/EmployeeControllerIT.java
@@ -8,9 +8,11 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import org.json.JSONException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -29,27 +31,86 @@ public class EmployeeControllerIT {
 	
 	@LocalServerPort
 	private int port;
-
-	@Test
-	public void test() {
 	
-		String url =  "http://localhost:"+port+"/api/employees";
-		url = url + "/33"; 
-		TestRestTemplate restTemplate = new TestRestTemplate();
-		
-		HttpHeaders headers = new HttpHeaders();
+	private TestRestTemplate restTemplate;
+	private HttpHeaders headers;
+	
+	@Before
+	public void before() {
+		restTemplate = new TestRestTemplate();
+		headers = new HttpHeaders();
 		
 		List<MediaType> mediaTypes =new ArrayList<>();
 		mediaTypes.add(MediaType.APPLICATION_JSON);
 		headers.setAccept( mediaTypes );
 		
-		HttpEntity<String> httpEntity = new HttpEntity<String>("parameters",headers);
-
-		ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET,httpEntity,String.class);
-		
-		System.out.println("response: " +response.getBody());
-		
-		assertTrue(response.getBody().contains("\"firstName\":\"Χρήστος\""));
 	}
 
+	@Test
+	public void retrieveEmployeeTest() throws JSONException {
+	
+		String retrieveEmployeePath = "/33";
+		String url = createUrlWithPort(retrieveEmployeePath); 
+		
+		HttpEntity<String> httpEntity = new HttpEntity<String>("parameters",headers);
+		ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET,httpEntity,String.class);
+
+		String expected = "{employeeId:33,firstName:Χρήστος}";
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+
+	}
+
+	@Test
+	public void retrieveEmployeesFromConnectedAttributeTest() throws JSONException {
+	
+		String retrieveEmployeePath = "/connected_attribute/39";
+		String url = createUrlWithPort(retrieveEmployeePath); 
+		
+		HttpEntity<String> httpEntity = new HttpEntity<String>("parameters",headers);
+		ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET,httpEntity,String.class);
+
+
+		String expected = "{employeeId:34}";
+		expected += ",{employeeId:39}";
+		expected += ",{employeeId:40}";
+		expected += ",{employeeId:42}";
+		expected += ",{employeeId:45}";
+		expected += ",{employeeId:48}";
+		expected += ",{employeeId:33}";
+		expected = "["+expected+"]";
+		
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+
+	}	
+	
+	@Test
+	public void retrieveEmployeesNotContainSpecificEmployee() throws JSONException {
+	
+		String retrieveEmployeePath = "/not_contnain_employee/39";
+		String url = createUrlWithPort(retrieveEmployeePath); 
+		
+		HttpEntity<String> httpEntity = new HttpEntity<String>("parameters",headers);
+		ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET,httpEntity,String.class);
+
+		String expected = "{employeeId:33}";
+		expected += ",{employeeId:34}";
+		expected += ",{employeeId:35}";
+		expected += ",{employeeId:36}";
+		expected += ",{employeeId:38}";
+		expected += ",{employeeId:40}";
+		expected += ",{employeeId:41}";
+		expected += ",{employeeId:42}";
+		expected += ",{employeeId:44}";
+		expected += ",{employeeId:45}";
+		expected += ",{employeeId:47}";
+		expected += ",{employeeId:48}";
+		expected = "["+expected+"]";
+		
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+
+	}	
+	private String createUrlWithPort(String retrieveEmployee) {
+		return "http://localhost:"+port+"/api/employees" + retrieveEmployee;
+	}
+ 
 }
